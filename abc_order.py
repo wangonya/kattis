ac64e3740357ec4a6c053b43308437b9382ba0b6
wangonya <kwangonya@gmail.com>
2019-09-05 12:11:46 +0300

    passes,fails on some test cases

--- abc_order.py
+++ abc_order.py
@@ -2,19 +2,17 @@
 import sys
 
 
-def abc(nums):
-    order = sys.stdin.readline().strip()
+def abc(nums, order):
     max_order_pos = order.index(max(order))
     min_order_pos = order.index(min(order))
     max_nums_pos = nums.index(max(nums))
     min_nums_pos = nums.index(min(nums))
-    nums[max_nums_pos], nums[min_nums_pos] = nums[max_order_pos], nums[min_order_pos]
-    print(f'{nums[max_nums_pos]}, {nums[min_nums_pos]} = '
-          f'{nums[max_order_pos]}, {nums[min_order_pos]}')
+    nums.insert(max_order_pos, nums.pop(max_nums_pos))
+    nums.insert(min_order_pos, nums.pop(min_nums_pos))
     print(' '.join(n for n in nums))
 
 
 if __name__ == '__main__':
-    # a, b, c, = sys.stdin.readline().split()
-    inp = list(map(str, sys.stdin.readline().split()))
-    abc(inp)
+    a, b, c, = sys.stdin.readline().split()
+    o = sys.stdin.readline().strip()
+    abc([a, b, c], o)
